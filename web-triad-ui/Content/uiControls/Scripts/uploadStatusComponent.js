afec2e2f57edaf8371472a3bf09fb68d2cb9d787
vromanenko <vromanenko@acr.org>
2017-08-28 19:57:42 +0300

    added showing the percentage progress

--- web-triad-ui/Content/uiControls/Scripts/uploadStatusComponent.js
+++ web-triad-ui/Content/uiControls/Scripts/uploadStatusComponent.js
@@ -1,30 +1,31 @@
 ﻿function UploadStatusComponent(container) {
     this._container = container;
 
-    this.showProgressBar = function() {
+    this.showProgressBar = function () {
         let self = this;
 
         self._container.empty();
-        self._container.append("<div class='tc-progress-bar tc-upload-progress'><span></span></div>");
+        self._container.append("<div class='tc-progress-bar tc-upload-progress'><span class='tc-upload-progress-val'></span><span class='tc-upload-progress-bar'></span></div>");
     }
 
-    this.updateProgressBar = function(processedValue) {
+    this.updateProgressBar = function (processedValue) {
         let self = this;
 
         if (self._progressBarMaxValue === null)
             throw new "Error. Progress bar component was not initialized. Please call showProgressBar function before updateProgressBar";
 
-        self._container.find(".tc-upload-progress span").width(processedValue + "%");
+        self._container.find(".tc-upload-progress > .tc-upload-progress-bar").width(processedValue + "%");
+        self._container.find(".tc-upload-progress > .tc-upload-progress-val").html(processedValue + "%");
     }
 
-    this.showStatus = function(status) {
+    this.showStatus = function (status) {
         let self = this;
 
         self._container.empty();
         self._container.html(status);
     }
 
-    this.showStatusWithRetryButton = function(status, retryCallback) {
+    this.showStatusWithRetryButton = function (status, retryCallback) {
         let self = this;
 
         self.showStatus(status);
